23a6d54ba797063acfcf952b4f2ba8cd57a19b8d
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-07-20 11:54:13 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+function addBatteries(total, batch) {
+  return total + batch;
+}
+
+const totalBatteries = batteryBatches.reduce(addBatteries, 0);
+
+console.log(totalBatteries);
+
+module.exports = {
+  batteryBatches,
+  totalBatteries
+};
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,19 +1,10 @@
-require ( './helpers.js' );
+const { expect } = require('chai');
+const { batteryBatches, totalBatteries } = require('../index');
 
-const sinon = require( 'sinon' )
-
-describe('reducer', function() {
-  describe('batteries', function() {
-    it('should have a `totalBatteries` variable', function() {
-      expect(totalBatteries).to.exist;
-    });
-
-    it('should have a number as a result', function() {
-      expect(totalBatteries).to.be.a('number');
-    });
-
-    it('should have made the sum of all the assembled batteries', function() {
-      expect(totalBatteries).to.eql(31);
+describe('index.js', function () {
+  describe('totalBatteries', function () {
+    it('returns the total number of batteries', function () {
+      expect(totalBatteries).to.equal(31);
     });
   });
 });
